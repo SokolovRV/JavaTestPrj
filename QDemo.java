d5e180eb629d202416724369f868e62a6946550e
RomanRV <sokolov.inf@gmail.com>
2019-09-10 15:43:09 +0300

    mod

--- QDemo.java
+++ QDemo.java
@@ -1,10 +1,27 @@
 class Queue {
-	char q[];
-	int putloc, getloc;
+	private char q[];
+	private int putloc, getloc;
 
 	Queue(int size) {
 		q=new char[size+1];
 		putloc = getloc = 0;
+	}
+
+	Queue(Queue ob) {
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q=new char[ob.length];
+
+		for(int i= getloc+1; i <= putloc; i++)
+			q[i] = ob.q[i];
+	}
+
+	Queue (char a[]) {
+		putloc = getloc = 0;
+		q = new char[a.length+1];
+
+		for(int i=0; i < a.length; i++)
+			put(a[i]);
 	}
 
 
